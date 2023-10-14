308ebdfc94a7cb88c3fa64ec9285a6cf48a79f54
Nicolas Cuffia-Bergeron <129567055+abxzok379@users.noreply.github.com>
2023-10-14 00:28:01 -0400

    Clean code

--- script.js
+++ script.js
@@ -1,33 +1,10 @@
-
-
-
-
-
-
-
-
-
-
-
 function startChrono(){
-//***********************************************-- Déclaration des variables --
-
-var z = 1000;
+  var z = 1000;
   for (let x = 0;x<1000;x++){
-   var timeout = setTimeout(retour,z);
-   z = z + 1000;
-function retour(){
-    document.querySelector("#chrono").innerHTML = x;
-  }
-
-
-
-}
-
-
-
-//**************************************************
-}
+    var timeout = setTimeout(retour,z);
+    z = z + 1000;
+  function retour(){ document.querySelector("#chrono").innerHTML = x;}
+}}
 
 
 
